e6b8bf51eea2e115eda674beaac086891f333497
Caio Santos <caio.santos687@gmail.com>
2019-05-08 22:26:34 -0300

    Init Project

---
+++ CrudPessoa/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CrudPessoa.Models;
+
+namespace CrudPessoa.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static IList<Pessoa> listaPessoas = new List<Pessoa>()
+        {
+            new Pessoa()
+            {
+                Nome = "Caio",
+                Idade = 29,
+                Dinheiro = Convert.ToDecimal(22.30),
+            },
+            new Pessoa()
+            {
+                Nome = "Cesar",
+                Idade = 29,
+                Dinheiro = Convert.ToDecimal(02.50),
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(listaPessoas);
+        }
+
+        public IActionResult CriarPessoaForm()
+        {
+            return View();
+        }
+        public IActionResult CriarPessoa(Pessoa pessoa)
+        {
+            listaPessoas.Add(pessoa);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Delete(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+        public IActionResult DeletarPessoa(Pessoa pessoa)
+        {
+            listaPessoas.Remove(pessoa);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Edit(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CrudPessoa/Models/PessoaModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudPessoa.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public decimal Dinheiro { get; set; }
+    }
+}
